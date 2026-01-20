27d88b6313580552f1c19bff24284315573e6a63
muhammad-iqbal-bin-zurimi <18DIT24F1001@student.ptss.edu.my>
2026-01-20 10:06:19 +0800

    Pisahkan kod kepada index.php, biodata.php dan style.css; tambah gaya CSS

---
+++ biodata.php
@@ -0,0 +1,4 @@
+<?php
+$nama = "MUHAMMAD IQBAL BIN ZURIMI";
+$nomatrik = "18DIT24F1001";
+$kelas = "DIT4B";
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-$nama = "MUHAMMAD IQBAL BIN ZURIMI";
-$nomatrik = "18DIT24F1001";
-$kelas = "DIT4B";
+include 'biodata.php';
 ?>
 
 <!DOCTYPE html>
@@ -9,19 +7,16 @@ $kelas = "DIT4B";
 <head>
     <meta charset="UTF-8">
     <title>Biodata Pelajar</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
-<body bgcolor="lightblue">
-
-<center>
+<body>
     <h1>Biodata Saya</h1>
 
-    <img src="gambar.jpeg" width="200" alt="Gambar Pelajar">
+    <img src="gambar.jpeg" alt="Gambar Pelajar">
 
-    <p><b>Nama:</b> <?= $nama ?></p>
-    <p><b>No Matrik:</b> <?= $nomatrik ?></p>
-    <p><b>Kelas:</b> <?= $kelas ?></p>
-</center>
-
+    <p>Nama: <?= $nama ?></p>
+    <p>No Matrik: <?= $nomatrik ?></p>
+    <p>Kelas: <?= $kelas ?></p>
 </body>
 </html>
