3b75c0c714d5b42a0387b4bc2cf17a3aaf13c5c6
muhammad-iqbal-bin-zurimi <18DIT24F1001@student.ptss.edu.my>
2026-01-20 10:04:45 +0800

    git add . && git commit -m "Pisahkan kod kepada index.php, biodata.php dan style.css; tambah gaya CSS"

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$nama = "MUHAMMAD IQBAL BIN ZURIMI";
+$nomatrik = "18DIT24F1001";
+$kelas = "DIT4B";
+?>
+
+<!DOCTYPE html>
+<html lang="ms">
+<head>
+    <meta charset="UTF-8">
+    <title>Biodata Pelajar</title>
+</head>
+
+<body bgcolor="lightblue">
+
+<center>
+    <h1>Biodata Saya</h1>
+
+    <img src="gambar.jpeg" width="200" alt="Gambar Pelajar">
+
+    <p><b>Nama:</b> <?= $nama ?></p>
+    <p><b>No Matrik:</b> <?= $nomatrik ?></p>
+    <p><b>Kelas:</b> <?= $kelas ?></p>
+</center>
+
+</body>
+</html>
